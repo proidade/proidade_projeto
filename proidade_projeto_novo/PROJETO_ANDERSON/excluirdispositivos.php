ecc0b6759ed5e6ca373c3741fa032f8b691397af
Rian Jalovitzki Pereira <rianjalovitzki@gmail.com>
2021-11-10 22:30:49 -0300

    Remoção de pasta

--- proidade_projeto_novo/PROJETO_ANDERSON/excluirdispositivos.php
+++
@@ -1,9 +0,0 @@
-<?php
-     include('conexaoPro.php');
-    $deletar = "DELETE FROM device WHERE id = '" . $_GET['id']."'";
-    echo $deletar;
-    $apagar = mysqli_query($connectionMysqlProage, $deletar);
-
-    echo '<script> window.location.href="listatDispositivos.php"; </script>';
-    
-?>
